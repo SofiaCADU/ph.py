89bd7ed77d21b463d8b4a0a37c7c5da6de7b4b7f
SofiaCADU <162449213+SofiaCADU@users.noreply.github.com>
2025-03-31 12:24:11 -0300

    Add files via upload

---
+++ ph.py
@@ -0,0 +1,12 @@
+# Sofía Cáceres
+# 31-03-2025
+# Estoy por cumplir 18
+
+ph = int(input('Ingresa el nivel de ph(0-14):'))
+
+if ph > 7:
+    print('Básico')
+elif ph < 7:
+    print('Acido')
+else:
+    print('Neutro')
